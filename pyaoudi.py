0ffacac0e43789c15671dcc9f0d79fa1bbd84a3d
allnc <meibrohimov@gmail.com>
2023-12-04 02:16:30 +0500

    new dile add

--- pyaoudi.py
+++ pyaoudi.py
@@ -5,10 +5,10 @@ URL = "https://uzxit.net/uzbek-mp3"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-
+audio: list[str] = []
 # job_elements = soup.find_all("div", class_="cols fx-row")
 data = soup.find_all("div", class_="track-item fx-row fx-middle js-item js-share-item")
 for x in data:
     data_track_value = x.get("data-track")
-
-    print("data-track:", data_track_value)
+    audio.append(data_track_value)
+    print(data_track_value)
