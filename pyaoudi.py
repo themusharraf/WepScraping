f565e1077b6ccac72dfb3da7487aea1259ccfda3
allnc <meibrohimov@gmail.com>
2023-12-04 02:14:19 +0500

    new dile add

--- pyaoudi.py
+++ pyaoudi.py
@@ -7,8 +7,8 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
 # job_elements = soup.find_all("div", class_="cols fx-row")
-job = soup.find_all("div", class_="track-item fx-row fx-middle js-item js-share-item")
-for x in job:
+data = soup.find_all("div", class_="track-item fx-row fx-middle js-item js-share-item")
+for x in data:
     data_track_value = x.get("data-track")
 
     print("data-track:", data_track_value)
